558d32958b5e16ba2691898292e8e2fc0f1e77c5
DimaHrist <d.khristyukha@wilix.org>
2019-10-30 02:57:00 +0300

    Get info from API

---
+++ weather-app/src/app/weather/services/weather.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  key = 'APPID=bd2f4622a58020871ca771d6faba1957';
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherByCity(city): Observable<any> {
+    return this
+      .http
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&${this.key}`
+      );
+  }
+
+}
--- weather-app/src/app/weather/weather.component.ts
+++ weather-app/src/app/weather/weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-weather',
@@ -7,9 +8,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  currentCity: string;
+  weatherStatus: string;
+  weather: any;
+  windSpeed: number;
+  windDeg: number;
+  windDirection: string;
+  pressure: number;
+  humidity: number;
+  clouds: number;
+  tempC: number;
+  tempF: number;
+
+  constructor( private weatherService: WeatherService) { }
 
   ngOnInit() {
+    this.getCurrentLocation();
   }
+
+  getCurrentLocation() {
+    this.getWeather();
+  }
+
+  getWeather() {
+    this.weatherService.getWeatherByCity('Moscow')
+    .subscribe(data => {
+      this.weather = data;
+      this.windSpeed = this.weather.wind.speed;
+      this.windDeg = this.weather.wind.deg;
+      this.pressure = Math.round(this.weather.main.pressure * 0.75);
+      this.humidity = this.weather.main.humidity;
+      this.clouds = this.weather.clouds.all;
+      this.tempC = Math.round(this.weather.main.temp - 273.15);
+      this.tempF = Math.round((this.weather.main.temp * (9 / 5)) - 459.67);
+      this.weatherStatus = this.weather.weather[0].main;
+      console.log(this.weatherStatus);
+      if (this.windDeg === 0 || this.windDeg === 360 )  {
+        this.windDirection = 'Северный';
+      } else if (this.windDeg > 0 && this.windDeg < 90) {
+        this.windDirection = 'Северо-Восточный';
+      } else if (this.windDeg === 90) {
+        this.windDirection = 'Восточный';
+      } else if (this.windDeg > 90 && this.windDeg < 180) {
+        this.windDirection = 'Юго-Восточный';
+      } else if (this.windDeg === 180) {
+        this.windDirection = 'Южный';
+      } else if (this.windDeg > 180 && this.windDeg < 270) {
+        this.windDirection = 'Юго-Западный';
+      } else if (this.windDeg === 270) {
+        this.windDirection = 'Западный';
+      } else if (this.windDeg > 270 && this.windDeg < 360) {
+        this.windDirection = 'Северо-Западный';
+      } else {
+        return;
+      }
+      console.log(this.windDirection);
+      
+    });
+  }
+  
+
 
 }
